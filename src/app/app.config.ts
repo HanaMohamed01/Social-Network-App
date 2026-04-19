7c6a55ad9fde95e9a9e9663a203c75b3bd9bd421
Hana Mohamed <elnegeryh@gmail.com>
2026-04-19 17:13:02 +0200

    feat: Implement authentication and main layout components with routing setup

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -14,7 +14,7 @@ export const appConfig: ApplicationConfig = {
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes, withInMemoryScrolling({
       scrollPositionRestoration: 'top',
-    }), withViewTransitions() , withHashLocation()),
+    }), withViewTransitions(), withHashLocation()),
     provideHttpClient(withFetch()),
   ],
 };
