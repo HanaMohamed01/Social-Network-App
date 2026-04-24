6af222adf951d8167084cfa0cf5e19cea816d542
Hana Mohamed <elnegeryh@gmail.com>
2026-04-25 02:52:10 +0300

    feat: implement registration component with reactive form validation

--- src/app/features/register/register.component.ts
+++ src/app/features/register/register.component.ts
@@ -1,11 +1,33 @@
 import { Component } from '@angular/core';
-
+import {
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 @Component({
   selector: 'app-register',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './register.component.html',
   styleUrl: './register.component.css',
 })
 export class RegisterComponent {
+  registerForm: FormGroup = new FormGroup({
+    name: new FormControl('', [Validators.required, Validators.minLength(3)]),
+    username: new FormControl(''),
+    email: new FormControl('', [Validators.required, Validators.email]),
+    dateOfBirth: new FormControl('', Validators.required),
+    gender: new FormControl('female', Validators.required),
+    password: new FormControl('', [
+      Validators.required,
+      Validators.pattern(
+        /^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,}$/,
+      ),
+    ]),
+    rePassword: new FormControl('', Validators.required),
+  });
 
+  submitForm(): void {
+    console.log(this.registerForm);
+  }
 }
