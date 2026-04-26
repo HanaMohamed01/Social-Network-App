cc7ac1ff1e4b3d0c5cc8b0815d5dc57b1f79b087
Hana Mohamed <elnegeryh@gmail.com>
2026-04-27 01:19:34 +0300

    feat: add password mismatch validation to registration form

--- src/app/features/register/register.component.ts
+++ src/app/features/register/register.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import {
+  AbstractControl,
   FormControl,
   FormGroup,
   ReactiveFormsModule,
@@ -12,24 +13,40 @@ import {
   styleUrl: './register.component.css',
 })
 export class RegisterComponent {
-  registerForm: FormGroup = new FormGroup({
-    name: new FormControl('', [Validators.required, Validators.minLength(3)]),
-    username: new FormControl(''),
-    email: new FormControl('', [Validators.required, Validators.email]),
-    dateOfBirth: new FormControl('', Validators.required),
-    gender: new FormControl('female', Validators.required),
-    password: new FormControl('', [
-      Validators.required,
-      Validators.pattern(
-        /^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,}$/,
-      ),
-    ]),
-    rePassword: new FormControl('', Validators.required),
-  });
+  registerForm: FormGroup = new FormGroup(
+    {
+      name: new FormControl('', [Validators.required, Validators.minLength(3)]),
+      username: new FormControl(''),
+      email: new FormControl('', [Validators.required, Validators.email]),
+      dateOfBirth: new FormControl('', Validators.required),
+      gender: new FormControl('female', Validators.required),
+      password: new FormControl('', [
+        Validators.required,
+        Validators.pattern(
+          /^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,}$/,
+        ),
+      ]),
+      rePassword: new FormControl('', Validators.required),
+    },
+    { validators: this.confirmPassword },
+  );
+
+  confirmPassword(group: AbstractControl) {
+    const password = group.get('password')?.value;
+    const rePassword = group.get('rePassword')?.value;
+
+    if (rePassword != password) {
+      group.get('rePassword')?.setErrors({ mismatch: true });
+
+      return { mismatch: true };
+
+    }
+    return null;
+  }
 
   submitForm(): void {
     if (this.registerForm.valid) {
-      console.log(this.registerForm);
+      console.log(this.registerForm.value);
     }
   }
 }
